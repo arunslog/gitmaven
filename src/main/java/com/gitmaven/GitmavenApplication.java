fe9ef2de01d0ac0b1d7347b8bf6a9312bd1a7a99
VENKATESAN <venkatesmail@gmail.com>
2023-05-21 18:33:59 +0530

    java commit

---
+++ src/main/java/com/gitmaven/GitmavenApplication.java
@@ -0,0 +1,13 @@
+package com.gitmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitmavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitmavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitmaven/GitmavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitmavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
