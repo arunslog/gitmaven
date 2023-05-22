16b187639881511dde810a5dc480839bc9043c79
VENKATESAN <venkatesmail@gmail.com>
2023-05-22 20:12:59 +0530

    added

--- src/main/java/com/gitmaven/GitmavenApplication.java
+++ src/main/java/com/gitmaven/GitmavenApplication.java
@@ -15,22 +15,27 @@ public class GitmavenApplication {
 		SpringApplication.run(GitmavenApplication.class, args);
 		List<Employee> employeeList =new ArrayList<>();
 		employeeList.add(new Employee(1,"arun45",45));
-		employeeList.add(new Employee(19,"arun",32));
+		employeeList.add(new Employee(19,"arun",55));
 		employeeList.add(new Employee(12,"venkat",30));
 		List<Employee> employeeStream=employeeList
 				.stream()
-				.sorted(Comparator.comparingInt(Employee::getSalary).reversed())
+				.sorted(Comparator.comparingInt(Employee::getSalary))
 				.collect(Collectors.toList());
 		List<Employee> employeeComparing=employeeList
 				.stream()
-				.sorted((o1,o2) -> o1.getSalary()- o2.getSalary()
-				)
+				.sorted((o1,o2) -> o1.getSalary()-o2.getSalary())
 				.collect(Collectors.toList());
 		employeeStream.forEach(System.out::println);
 		employeeComparing.forEach(System.out::println);
 
 
-		Function< Employee, ?> employee = (Function<Employee, Object>) employee1 -> employee1;
+		//Function< Employee, ?> employee = (Function<Employee, Object>) employee1 -> employee1;
+		Function<? super Employee, ?> employee = new Function<Employee, Object>() {
+			@Override
+			public Object apply(Employee employee) {
+				return employee;
+			}
+		};
 		Map empMap= employeeList.stream().collect(Collectors.toMap(Employee::getId,employee));
 
 		System.out.println("map"+empMap);
