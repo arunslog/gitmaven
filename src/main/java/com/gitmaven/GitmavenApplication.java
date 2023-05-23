ca53b7563f88d5c68e89d4ee4591780c174d673f
VENKATESAN <venkatesmail@gmail.com>
2023-05-23 08:25:31 +0530

    merge onflit

--- src/main/java/com/gitmaven/GitmavenApplication.java
+++ src/main/java/com/gitmaven/GitmavenApplication.java
@@ -1,5 +1,6 @@
 package com.gitmaven;
 
+import lombok.val;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,17 +14,17 @@ public class GitmavenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitmavenApplication.class, args);
-		List<Employee> employeeList =new ArrayList<>();
-		employeeList.add(new Employee(1,"arun45",45));
-		employeeList.add(new Employee(19,"arun",55));
-		employeeList.add(new Employee(12,"venkat",30));
-		List<Employee> employeeStream=employeeList
+		List<Employee> employeeList = new ArrayList<>();
+		employeeList.add(new Employee(1, "arun45", 45));
+		employeeList.add(new Employee(19, "arun", 55));
+		employeeList.add(new Employee(12, "venkat", 30));
+		List<Employee> employeeStream = employeeList
 				.stream()
 				.sorted(Comparator.comparingInt(Employee::getSalary))
 				.collect(Collectors.toList());
-		List<Employee> employeeComparing=employeeList
+		List<Employee> employeeComparing = employeeList
 				.stream()
-				.sorted((o1,o2) -> o1.getSalary()-o2.getSalary())
+				.sorted((o1, o2) -> o1.getSalary() - o2.getSalary())
 				.collect(Collectors.toList());
 		employeeStream.forEach(System.out::println);
 		employeeComparing.forEach(System.out::println);
@@ -36,12 +37,22 @@ public class GitmavenApplication {
 				return employee;
 			}
 		};
-		Map empMap= employeeList.stream().collect(Collectors.toMap(Employee::getId,employee));
+		Map empMap = employeeList.stream().collect(Collectors.toMap(Employee::getId, employee));
 
-		System.out.println("map"+empMap);
+		System.out.println("map" + empMap);
 
-		List<? super Number> loweBounds=new ArrayList<>();
+		List<? super Number> loweBounds = new ArrayList<>();
 		loweBounds.add(4);
-	}
 
+		//3 apple, 2 banana, others 1
+		val items =
+				Arrays.asList("apple", "apple", "banana",
+						"apple", "orange", "banana", "papaya");
+
+		val result =
+				items.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		System.out.printf("Results " + result);
+
+
+	}
 }
